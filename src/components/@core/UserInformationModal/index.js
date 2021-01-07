805e1622ba4ce347c29bc872336d4fcfbd1c6144
ngoctin02806 <lightghost345@gmail.com>
2021-01-07 10:19:22 +0700

    feat: load user profile completely

--- src/components/@core/UserInformationModal/index.js
+++ src/components/@core/UserInformationModal/index.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { Modal, Avatar, Divider, Typography } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
+import { Modal, Avatar, Divider, Typography, Badge } from "antd";
+import {
+  UserOutlined,
+  MoneyCollectOutlined,
+  ArrowUpOutlined,
+  ArrowDownOutlined,
+} from "@ant-design/icons";
+import { useParams } from "react-router-dom";
+
+import axios from "axios";
 
 const { Text } = Typography;
 
 const UserInformationModal = (props) => {
   const { isModalVisible, handleCancel, handleOk } = props;
+
+  const [userProfile, setUserProfile] = useState({});
+
+  const { userId } = useParams();
+
+  useEffect(() => {
+    document.title = userProfile.user
+      ? userProfile.user.username
+      : "Trang chủ Game";
+  }, [userProfile]);
+
+  useEffect(() => {
+    axios(`/users/${userId}/profile`, {
+      method: "GET",
+    })
+      .then((res) => {
+        setUserProfile({ ...userProfile, ...res.data });
+      })
+      .catch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Modal
@@ -16,16 +45,36 @@ const UserInformationModal = (props) => {
       closable={false}
       style={{ position: "relative" }}
     >
-      <Avatar
+      <div
         style={{
           position: "absolute",
           top: "-35px",
           left: "50%",
           transform: "translateX(-50%)",
         }}
-        size={64}
-      />
-      <Divider />
+      >
+        <Badge dot={true} offset={[-10, 55]} color="green">
+          <Avatar
+            style={{ border: "2px solid #fff" }}
+            size={64}
+            src={userProfile.user && userProfile.user.avatar}
+          />
+        </Badge>
+      </div>
+      <div style={{ textAlign: "center", marginTop: "5px" }}>
+        <Text strong>
+          Tỉ lệ thắng:{" "}
+          {userProfile.number_of_wins + userProfile.number_of_loses !== 0
+            ? (
+                (userProfile.number_of_wins /
+                  (userProfile.number_of_wins + userProfile.number_of_loses)) *
+                100
+              ).toFixed(2)
+            : 0}{" "}
+          %
+        </Text>
+      </div>
+      <Divider style={{ margin: "10px 0px 15px 0px" }} />
       <div
         style={{
           display: "flex",
@@ -34,15 +83,24 @@ const UserInformationModal = (props) => {
         }}
       >
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <Text>
-            <UserOutlined />
-            Tên: Nguyễn Văn A
+          <Text strong>
+            <UserOutlined style={{ marginRight: "5px" }} />
+            Tên: {userProfile.user && userProfile.user.username}
           </Text>
-          <Text>Cup: 123</Text>
+          <Text strong>
+            <MoneyCollectOutlined style={{ marginRight: "5px" }} />
+            Số lượng cup: {userProfile.point}
+          </Text>
         </div>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <Text>Thắng: 12</Text>
-          <Text>Thua: 3</Text>
+          <Text strong>
+            <ArrowUpOutlined style={{ marginRight: "5px" }} />
+            Thắng: {userProfile.number_of_wins}
+          </Text>
+          <Text strong>
+            <ArrowDownOutlined style={{ marginRight: "5px" }} />
+            Thua: {userProfile.number_of_loses}
+          </Text>
         </div>
       </div>
     </Modal>
--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const NODE_ENV = "development";
 
 if (NODE_ENV === "development") {
-  axios.defaults.baseURL = "http://192.168.1.20:8000/api/v1";
+  axios.defaults.baseURL = "http://localhost:8000/api/v1";
 } else {
   axios.defaults.baseURL = "http://api.hcmusedu.info/api/v1";
 }
