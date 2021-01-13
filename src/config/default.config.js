12808e04403dc047c2b9fff389ece277036808d3
ngoctin02806 <lightghost345@gmail.com>
2021-01-13 17:23:43 +0700

    chore: change host to reset password

--- src/config/default.config.js
+++ src/config/default.config.js
@@ -2,7 +2,7 @@ const config = {
   GOOGLE_CLIENT_ID:
     "275202630194-579h9co7coe7jc0rpjd6rvie7rcndgfb.apps.googleusercontent.com",
   FACEBOOK_CLIENT_ID: "2605705256387595",
-  HOST_FE: "http://localhost:3000",
+  HOST_FE: "https://www.hcmusedu.info",
 };
 
 export default config;
